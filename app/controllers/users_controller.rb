e4838e5d2d2e6d606cb707ec57f813d15598767e
acmenes <acmenescompositions@gmail.com>
2016-02-10 05:52:28 +0000

    a lot of work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
    
    def my_portfolio
-   
+      @user_stocks = current_user.stocks #bc of many-to-many association
+      @user = current_user
    end 
    
 end    
