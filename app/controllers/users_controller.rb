5463257d76cfd7aebfdc1122cc9b60488b0852e9
acmenes <acmenescompositions@gmail.com>
2016-02-11 17:43:38 +0000

    add friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
    def my_portfolio
       @user_stocks = current_user.stocks #bc of many-to-many association
       @user = current_user
-   end 
+   end
+   
+   def my_friends
+      
+   end   
    
 end    
